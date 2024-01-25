a32ebe7a890a86941f4076553870a7ada9df6c08
Stefan Klessinger <stefan.klessinger@posteo.de>
2024-01-25 18:36:13 +0100

    Make data cleaning script more robust and add support for timeouts

--- artifacts/charts/clean_data.r
+++ artifacts/charts/clean_data.r
@@ -1,12 +1,23 @@
 # Remove cancelled runs from dataset
 
-gen_df <- function(in_path, out_path) {
-  df  <- read.csv(in_path, sep=",")
-  clean_df <- df[(is.na(df$cancelledAt) | df$cancelledAt==""),]
-  write.csv(clean_df,out_path, row.names = FALSE)
+gen_df <- function(in_path, out_path, write_timeouts=FALSE) {
+  if (file.exists(in_path)) {
+    print(paste("Cleaning file:", in_path))
+    df <- read.csv(in_path, sep=",")
+    clean_df <- df[(is.na(df$cancelledAt) | df$cancelledAt==""),]
+    write.csv(clean_df,paste(out_path,"clean_data.csv",sep="/"), row.names = FALSE)
+    if (write_timeouts) {
+      # get all rows with "TimeoutException" in any column except the first
+      timeout_df <- df[apply(df[,-1], 1, function(x) any(grepl("TimeoutException", x))),]
+      # write to timeout.csv in out_path directory
+      write.csv(timeout_df,paste(out_path,"timeout.csv",sep="/"), row.names = FALSE)
+    }
+  } else {
+    print(paste("File not found:", in_path))
+  }
 }
 
-gen_df('data/github/results.csv', 'data/github/clean_data.csv')
-gen_df('data/kubernetes/results.csv', 'data/kubernetes/clean_data.csv')
-gen_df('data/snowplow/results.csv', 'data/snowplow/clean_data.csv')
-gen_df('data/wp/results.csv', 'data/wp/clean_data.csv')
+gen_df('data/github/results.csv', 'data/github/')
+gen_df('data/kubernetes/results.csv', 'data/kubernetes/')
+gen_df('data/snowplow/results.csv', 'data/snowplow/')
+gen_df('data/wp/results.csv', 'data/wp/')
